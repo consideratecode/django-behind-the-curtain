defa75c3c340fd315955160ce61b3f941d6496b3
Daniel Hepper <daniel.hepper@epicco.de>
2018-05-26 12:01:57 +0200

    Added default project

---
+++ default/server.py
@@ -0,0 +1,7 @@
+from wsgiref.simple_server import make_server
+from default.wsgi import application
+
+httpd = make_server('127.0.0.1', 8000, application)
+print("Serving HTTP on port 8000...")
+
+httpd.handle_request()
